2da2cc1aaf03e41fd6236279bdef0eb4a2d89b1f
Valentin <valentin.monteiro@orange.fr>
2024-07-11 12:32:00 +0200

    Création de fixtures pour User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('john.doe@example.fr')
+        ->setPassword('password')
+        ->setRoles(['ROLE_USER']);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
