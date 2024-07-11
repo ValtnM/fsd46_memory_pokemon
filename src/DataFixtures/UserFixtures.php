8c22661dcc4bc999901a85bb05ca322429b0abcf
Valentin <valentin.monteiro@orange.fr>
2024-07-11 12:55:01 +0200

    Création du système d'authentification

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,14 +5,22 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user->setEmail('john.doe@example.fr')
-        ->setPassword('password')
+        ->setPassword($this->hasher->hashPassword($user, "password"))
         ->setRoles(['ROLE_USER']);
         $manager->persist($user);
 
